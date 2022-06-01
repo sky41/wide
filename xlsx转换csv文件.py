918937495330fd361f35832b03b381fc8bf0b3da
sky41 <sky1417167991@163.com>
2022-06-01 11:03:39 +0800

    添加 README.md 文件

--- xlsx转换csv文件.py
+++ xlsx转换csv文件.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 
 def xlsx_to_csv_pd():
-    data_xls = pd.read_excel('附件_一季度.xlsx', index_col=0)  # 输入xlsx文件名
-    data_xls.to_csv('2.csv', encoding='utf-8')  # 输出csv文件名
+    data_xls = pd.read_excel('附件_三季度.xlsx', index_col=0)  # 输入xlsx文件名
+    data_xls.to_csv('附件03数据.csv', encoding='utf-8')  # 输出csv文件名
 
 
 if __name__ == '__main__':
